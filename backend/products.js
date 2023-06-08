06857f5f3c9f09a166c2ebf07ce20e3328254911
‪murtada kodi‬‏ <murtadakodi69@gmail.com>
2023-06-08 19:50:09 +0300

    lesson35-1

---
+++ backend/products.js
@@ -0,0 +1,61 @@
+const products = [
+  {
+    id: 1,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 100,
+    imageLink:
+[
+    'https://res.cloudinary.com/dmklduciw/image/upload/v1686134585/Online%20Store/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_qszmmc.jpg',
+    'https://res.cloudinary.com/dmklduciw/image/upload/v1686134585/Online%20Store/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_1_fw3v4u.jpg',
+    'https://res.cloudinary.com/dmklduciw/image/upload/v1686134584/Online%20Store/1/Mens_NASA_Space_Bear_Print_O-Neck_Casual_Loose_Short_Sleeve_T-Shirt_pugkj6.png',
+    'https://res.cloudinary.com/dmklduciw/image/upload/v1686134584/Online%20Store/1/NASA_Space_Bear_Print_T-Shirt_kszxxn.jpg'
+],
+  },
+  {
+    id: 2,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 200,
+    imageLink: 
+    [
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134585/Online%20Store/2/1658329192737168ff6ad063452563108e1de4d132_thumbnail_900x_wtkec6.webp',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134584/Online%20Store/2/164802324294a9e00cd18e3bdacdb07faadc6ea08e_thumbnail_900x_esdgku.webp',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134584/Online%20Store/2/1646271078e49bf65969984162cd89e52f225c1251_thumbnail_900x_lwsv65.webp',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134584/Online%20Store/2/1648023235ba32327d7aecda5b9de9913efa316d9e_thumbnail_900x_vahhpl.webp',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134584/Online%20Store/2/16480232420809ac54cac58a758d88fbee070f7f7a_thumbnail_900x_bh3sxy.webp'
+
+  ],  
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 300,
+    imageLink:
+    [
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134586/Online%20Store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_White___XL_xonrxf.png',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134585/Online%20Store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Khaki___XL_wzidna.png',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134585/Online%20Store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Grey___XL_gpenu3.png',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134585/Online%20Store/3/Paper_Plane_of_Liberty_Print_Crew_Neck_Short_Sleeve_T-Shirts_-_Black___XL_mjqg6r.png'
+  ],  
+  },
+  {
+    id: 4,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink:
+    [
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134586/Online%20Store/4/1646880634f3ecf23038717f7adbcf3658effab2e7_thumbnail_900x_odl0fn.webp',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134586/Online%20Store/4/1646822799c97e5f9754d3b3314b4e6ffd848ad22d_thumbnail_900x_ldfvc7.webp',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134586/Online%20Store/4/163826656927114e9da27ea115f55404141ba11264_thumbnail_900x_gcw7cg.webp',
+      'https://res.cloudinary.com/dmklduciw/image/upload/v1686134586/Online%20Store/4/164430580182db3b5a5192e660a87c832a397044b6_thumbnail_900x_ehcoxr.webp'
+  ],  
+  },
+];
+module.exports = products;
---
+++ frontend/src/pages/details/product-details.jsx
@@ -0,0 +1,74 @@
+import React, { useRef, useState } from "react";
+import "./product-details.css";
+import { useGetOneProductQuery } from "Redux/productsApi";
+import { useParams } from "react-router-dom";
+import { Box, CircularProgress, Typography } from "@mui/material";
+import DetailsThumb from "./DetailsThumb";
+const ProductDetails = () => {
+  let { id } = useParams();
+  const { data, error, isLoading } = useGetOneProductQuery(id);
+
+  const [index, setindex] = useState(0);
+  const myRef = useRef(null);
+
+  const handleTab = (index) => {
+    // this.setState({index: index})
+    setindex(index)
+    const images = myRef.current.children;
+    for(let i=0; i<images.length; i++){
+      images[i].className = images[i].className.replace("active", "");
+    }
+    images[index].className = "active";
+    
+  }
+
+  if (isLoading) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <Typography variant="h1" color="error">
+          {" "}
+          ERROR{" "}
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (data) {
+    return (
+      <div className="app details-page">
+        <div className="details">
+          <div className="big-img">
+            <img src={data.imageLink[index]} alt="" />
+          </div>
+
+          <div className="box">
+            <div className="row">
+              <h2>{data.productName}</h2>
+              <span>${data.price}</span>
+            </div>
+            {/*<Colors colors={item.colors} />*/}
+
+            <p>{data.description}</p>
+
+            <DetailsThumb
+              images={data.imageLink}
+              tab={handleTab}
+              myRef={myRef}
+            />
+            <button className="cart">Add to cart</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default ProductDetails;
